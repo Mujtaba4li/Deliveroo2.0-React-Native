bbba42afa5c7f4acd07e4d15b21e81d036ae1922
Mujtaba Ali <mujiroy01@gmail.com>
2022-09-19 15:23:45 +0500

    React Navigation install successfully

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
 import * as React from 'react';
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
 
-
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View >
+    <NavigationContainer>
+    {/* <View >
       <Text className=' text-red-600 mt-9 left-6'>
         Oh ustadd its working...
       </Text>
       
   
       <StatusBar style="auto" />
-    </View>
+    </View> */}
+     <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
 
+      </Stack.Navigator>
+    </NavigationContainer>
 
   );
 }
