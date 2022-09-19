56b49e53fd35efbcb5adb13ab331a270cc52f6fa
Mujtaba Ali <mujiroy01@gmail.com>
2022-09-19 17:30:55 +0500

    Header Remove SafeArea Added

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,21 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView } from "react-native";
+import React,{useLayoutEffect} from "react";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 
 export default function HomeScreen() {
-
-
+  const navigation = useNavigation();
+ 
+  useLayoutEffect(() => {
+    navigation.setOptions({
+    //   headerTitle: "Mujtaba",
+    headerShown:false,
+    });
+  }, []);
   return (
+    <SafeAreaView>
     <View>
-      <Text className=' text-red-700'>HomeScreen</Text>
+      <Text className=" text-red-700">HomeScreen</Text>
     </View>
-  )
+    </SafeAreaView>
+  );
 }
