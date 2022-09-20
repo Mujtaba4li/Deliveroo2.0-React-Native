9b1c17223653914eb602f86347eb395631b66a9a
Mujtaba Ali <mujiroy01@gmail.com>
2022-09-20 15:21:42 +0500

    Pic and text written

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,21 +1,31 @@
-import { View, Text, SafeAreaView } from "react-native";
-import React,{useLayoutEffect} from "react";
+import { View, Text, SafeAreaView, Image } from "react-native";
+import React, { useLayoutEffect } from "react";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
 
 export default function HomeScreen() {
   const navigation = useNavigation();
- 
+
   useLayoutEffect(() => {
     navigation.setOptions({
-    //   headerTitle: "Mujtaba",
-    headerShown:false,
+      //   headerTitle: "Mujtaba",
+      headerShown: false,
     });
   }, []);
   return (
     <SafeAreaView>
-    <View>
-      <Text className=" text-red-700">HomeScreen</Text>
-    </View>
+      <View className='flex-row space-x-2 align-middle ml-2'>
+        
+        <Image
+          source={{
+            uri: "https://mujtaba-ali.vercel.app/static/media/header-img.1c0118c3bc802a747396.png",
+          }}
+          className="h-8 w-8 rounded-full bg-emerald-500 p-5"
+        />
+        <View>
+          <Text className=' text-slate-500 text-xs'>Deliver Now!</Text>
+          <Text className='text-lg font-bold'>Current Location</Text>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
