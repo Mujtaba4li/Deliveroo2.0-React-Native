20382939e5b64f8a8a64ab8111e99d5f62523b61
Mujtaba Ali <mujiroy01@gmail.com>
2022-09-22 14:04:04 +0500

    Working on header

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,8 @@
 import { View, Text, SafeAreaView, Image } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
-
+import {SparklesIcon} from "react-native-heroicons/outline";
+import { FaBeer } from 'react-icons/fa';
 export default function HomeScreen() {
   const navigation = useNavigation();
 
@@ -13,8 +14,7 @@ export default function HomeScreen() {
   }, []);
   return (
     <SafeAreaView>
-      <View className='flex-row space-x-2 align-middle ml-2'>
-        
+      <View className="flex-row space-x-3 space-y-0 align-middle ml-2">
         <Image
           source={{
             uri: "https://mujtaba-ali.vercel.app/static/media/header-img.1c0118c3bc802a747396.png",
@@ -22,9 +22,11 @@ export default function HomeScreen() {
           className="h-8 w-8 rounded-full bg-emerald-500 p-5"
         />
         <View>
-          <Text className=' text-slate-500 text-xs'>Deliver Now!</Text>
-          <Text className='text-lg font-bold'>Current Location</Text>
+          <Text className="text-slate-500 text-xs">Deliver Now!</Text>
+          <Text className="text-lg font-bold">Current Location</Text>
+          <SparklesIcon/>
         </View>
+          
       </View>
     </SafeAreaView>
   );
