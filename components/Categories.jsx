67145b9bc7d531a00ea7299da8817784401ec05e
Mujtaba Ali <mujiroy01@gmail.com>
2022-09-24 12:17:02 +0500

    Improve little bit categries css and add full images

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -8,17 +8,22 @@ const Categories = () => {
     horizontal
     contentContainerStyle={{
         paddingHorizontal:15,
-        paddingTop:6
+        paddingTop:6,
+        
+
         
     }}
     >
-        <CategoryCard imgURL='https://mujtaba-ali.vercel.app/static/media/header-img.1c0118c3bc802a747396.png' Title='Title 1'/>
-        <CategoryCard imgURL='https://mujtaba-ali.vercel.app/static/media/header-img.1c0118c3bc802a747396.png' Title='Title 2'/>
-        <CategoryCard imgURL='https://mujtaba-ali.vercel.app/static/media/header-img.1c0118c3bc802a747396.png' Title='Title 3'/>
-        <CategoryCard imgURL='https://mujtaba-ali.vercel.app/static/media/header-img.1c0118c3bc802a747396.png' Title='Title 3'/>
-        <CategoryCard imgURL='https://mujtaba-ali.vercel.app/static/media/header-img.1c0118c3bc802a747396.png' Title='Title 3'/>
-        <CategoryCard imgURL='https://mujtaba-ali.vercel.app/static/media/header-img.1c0118c3bc802a747396.png' Title='Title 3'/>
-        <CategoryCard imgURL='https://mujtaba-ali.vercel.app/static/media/header-img.1c0118c3bc802a747396.png' Title='Title 3'/>
+        <CategoryCard imgURL='https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80' Title='Title 1'/>
+        <CategoryCard imgURL='https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80' Title='Title 1'/>
+        <CategoryCard imgURL='https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80' Title='Title 1'/>
+        <CategoryCard imgURL='https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80' Title='Title 1'/>
+        <CategoryCard imgURL='https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80' Title='Title 1'/>
+        <CategoryCard imgURL='https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80' Title='Title 1'/>
+        <CategoryCard imgURL='https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80' Title='Title 1'/>
+        <CategoryCard imgURL='https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80' Title='Title 1'/>
+        <CategoryCard imgURL='https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80' Title='Title 1'/>
+        <CategoryCard imgURL='https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80' Title='Title 1'/>
        
 
     </ScrollView>
--- components/CategoryCard.jsx
+++ components/CategoryCard.jsx
@@ -10,7 +10,7 @@ const CategoryCard = ({imgURL,Title}) => {
         }}
         className="h-20 w-20 rounded opacity-80"
         />
-      <Text className='absolute bottom-1 text-white font-bold'>{Title}</Text>
+      <Text className='absolute bottom-1 text-white font-bold left-4'>{Title}</Text>
 
     </TouchableOpacity>
   )
