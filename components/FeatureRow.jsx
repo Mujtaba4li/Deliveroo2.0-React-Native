30f1751f0379a5eaa9070a947140f0ef37ac3bdd
Mujtaba Ali <mujiroy01@gmail.com>
2022-09-24 20:51:51 +0500

    FeatureRow is complete now start  content that will inside it

--- components/FeatureRow.jsx
+++ components/FeatureRow.jsx
@@ -1,15 +1,16 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import { FontAwesome5 } from "@expo/vector-icons";
 const FeatureRow = (props) => {
   return (
-    <View className="p-4">
-      <View className='flex-row items-center'>
-        <Text>{props.title}</Text>
-        <FontAwesome5 name="arrow-right" size={18} color="#00CCBB"/>
+    <ScrollView className="p-4">
+      <View className='flex-row items-center justify-between'>
+        <Text className='font-extrabold'>{props.title}</Text>
+        <FontAwesome5 name="arrow-right"  size={18} color="#00CCBB"/>
       </View>
-      <Text>{props.description}</Text>
-    </View>
+
+      <Text className='text-xs text-gray-400'>{props.description}</Text>
+    </ScrollView>
   );
 };
 
