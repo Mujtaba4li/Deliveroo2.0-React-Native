4e0ff08c09c8c74f6702b7c97f7b6bdc34537005
Mujtaba Ali <mujiroy01@gmail.com>
2022-09-24 12:02:57 +0500

    Static Categories Donw

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -6,11 +6,20 @@ const Categories = () => {
   return (
     <ScrollView
     horizontal
-    className='m-2'
+    contentContainerStyle={{
+        paddingHorizontal:15,
+        paddingTop:6
+        
+    }}
     >
-        <CategoryCard/>
-        <CategoryCard/>
-        <CategoryCard/>
+        <CategoryCard imgURL='https://mujtaba-ali.vercel.app/static/media/header-img.1c0118c3bc802a747396.png' Title='Title 1'/>
+        <CategoryCard imgURL='https://mujtaba-ali.vercel.app/static/media/header-img.1c0118c3bc802a747396.png' Title='Title 2'/>
+        <CategoryCard imgURL='https://mujtaba-ali.vercel.app/static/media/header-img.1c0118c3bc802a747396.png' Title='Title 3'/>
+        <CategoryCard imgURL='https://mujtaba-ali.vercel.app/static/media/header-img.1c0118c3bc802a747396.png' Title='Title 3'/>
+        <CategoryCard imgURL='https://mujtaba-ali.vercel.app/static/media/header-img.1c0118c3bc802a747396.png' Title='Title 3'/>
+        <CategoryCard imgURL='https://mujtaba-ali.vercel.app/static/media/header-img.1c0118c3bc802a747396.png' Title='Title 3'/>
+        <CategoryCard imgURL='https://mujtaba-ali.vercel.app/static/media/header-img.1c0118c3bc802a747396.png' Title='Title 3'/>
+       
 
     </ScrollView>
   )
--- components/CategoryCard.jsx
+++ components/CategoryCard.jsx
@@ -1,11 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 
-const CategoryCard = () => {
+const CategoryCard = ({imgURL,Title}) => {
   return (
-    <View>
-      <Text>CategoryCard</Text>
-    </View>
+    <TouchableOpacity className='relative m-1'>
+        <Image
+        source={{
+            uri:imgURL
+        }}
+        className="h-20 w-20 rounded opacity-80"
+        />
+      <Text className='absolute bottom-1 text-white font-bold'>{Title}</Text>
+
+    </TouchableOpacity>
   )
 }
 
