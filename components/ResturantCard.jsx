acd96739ed63e4a9fee2b2359531f40829b42e17
Mujtaba Ali <mujiroy01@gmail.com>
2022-09-24 21:14:29 +0500

    JSX completed now start work on CSS

--- components/ResturantCard.jsx
+++ components/ResturantCard.jsx
@@ -1,25 +1,42 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import { AntDesign, Entypo } from "@expo/vector-icons";
+import React from "react";
 
-const ResturantCard = (
-    {
-        id,
-        imgURL,
-        title,
-        rating,
-        gendre,
-        short_descriptiom,
-        address,
-        dishes,
-        long,
-        lat
-    }
-) => {
+const ResturantCard = ({
+  id,
+  imgURL,
+  title,
+  rating,
+  gendre,
+  short_description,
+  address,
+  dishes,
+  long,
+  lat,
+}) => {
   return (
-    <View>
-      <Text>ResturantCard</Text>
-    </View>
-  )
-}
+    <TouchableOpacity>
+      <Image
+        source={{
+          uri: imgURL,
+        }}
+        className="h-36 w-56 rounded"
+      />
+      <Text>{title}</Text>
+      <View>
+        <Text>
+          <AntDesign name="star" size={24} color="black" />
+        </Text>
+        <Text>{rating}</Text>
+      </View>
+      <View>
+        <Text>
+          <Entypo name="location-pin" size={24} color="black" />
+        </Text>
+        <Text>Nearby • {address}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
-export default ResturantCard
+export default ResturantCard;
