c4f1188de83c8d5fce1ef66b803dfe86a7c10c7a
Mujtaba Ali <mujiroy01@gmail.com>
2022-09-24 11:44:11 +0500

    Working on Categories Card

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,11 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import CategoryCard from './CategoryCard'
 
 const Categories = () => {
   return (
-    <View>
-      <Text>Categories</Text>
-    </View>
+    <ScrollView
+    horizontal
+    className='m-2'
+    >
+        <CategoryCard/>
+        <CategoryCard/>
+        <CategoryCard/>
+
+    </ScrollView>
   )
 }
 
